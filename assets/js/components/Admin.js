1ea2ddab7441e0e83d7ca5f126d718568a2845e9
yemiwebby <yemiwebby@gmail.com>
2018-09-27 16:35:12 +0100

    Cleaned up admin component

--- assets/js/components/Admin.js
+++ assets/js/components/Admin.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import ReviewsBoard from './ReviewsBoard';
 import ReviewsList from './ReviewsList';
-import StarRatingComponent from 'react-star-rating-component';
-
-
 import { observer, inject } from "mobx-react";
-import { decorate, observable, action, computed } from "mobx";
-// import Review from '../Store/Store';
-import stores from '../stores';
-
-//
-// decorate(Review, {
-//     reviewList: observable,
-//     addReview: action,
-//     averageScore: computed,
-//     reviewCount: computed,
-//     fetchList: observable
-// });
-
-// const reviewStore = new Review();
-
-const List = (props) => {
-    return (
-        <li className="list-group-item">
-            <div className="float-left">{props.data.review}</div>
-            <div className="float-right">
-                <StarRatingComponent name="reviewRate" starCount={props.data.stars} />
-            </div>
-        </li>
-    )
-}
-
 
 @inject("reviews")
 @observer
@@ -39,7 +10,7 @@ class Admin extends React.Component {
         return(
             <div className={"container"}>
                 <div style={{ marginTop: 70}}>
-                    {/*<ReviewsBoard store={this.props}/>*/}
+                    <ReviewsBoard store={this.props}/>
                     <ReviewsList store={this.props}/>
                 </div>
             </div>

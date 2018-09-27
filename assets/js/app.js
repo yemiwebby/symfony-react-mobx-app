00d75a4210f7c7b05b36694e1adc829c98b6f25c
yemiwebby <yemiwebby@gmail.com>
2018-09-27 16:22:39 +0100

    Removed redundant modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter, NavLink, Route, Switch, Link, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import ReviewForm from './components/ReviewForm';
-import ReviewsBoard from './components/ReviewsBoard';
 import Admin from './components/Admin';
 import { Provider } from 'mobx-react';
 import stores from "./stores";
